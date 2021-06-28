aa19d50220607799f9cd588f5bc0f24e6e888ac0
fazza7777 <aungbarlaay7777@gmail.com>
2021-06-28 16:21:21 +0630

    register process

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,8 +1,90 @@
 <template>
-    <h1>Register</h1>
+ <div class="container my-5">
+   <div class="row justify-content-center">
+       <div class="col-lg-6 col-12">
+           <div class="card shadow">
+                <div class="card-header bg-primary ">
+                    <h3 class="text-white">Register</h3>
+                </div>
+                <div class="card-body px-5">
+                    <form @submit.prevent="register">
+                        <div class="form-group">
+                            <label for="">Name</label>
+                            <input type="text" v-model="creditial.name" :class="{'is-invalid':errors.name}"  class="form-control">
+                            <small v-if="errors.name" class="text text-danger"><strong>{{errors.name[0]}}</strong></small>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                               <div class="form-group">
+                                    <label for="">Phone</label>
+                                    <input type="number" v-model="creditial.phone" :class="{'is-invalid':errors.phone}"  class="form-control">
+                                    <small v-if="errors.phone" class="text text-danger"><strong>{{errors.phone[0]}}</strong></small>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                               <div class="form-group">
+                                    <label for="">Email</label>
+                                    <input type="email" v-model="creditial.email" :class="{'is-invalid':errors.email}" class="form-control">
+                                    <small v-if="errors.email" class="text text-danger"><strong>{{errors.email[0]}}</strong></small>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="form-group">
+                                        <label for="">Password</label>
+                                        <input type="password" :class="{'is-invalid':errors.password}" v-model="creditial.password" class="form-control">
+                                        <small v-if="errors.password" class="text text-danger"><strong>{{errors.password[0]}}</strong></small>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                               <div class="form-group">
+                                    <label for="">Password Confirm</label>
+                                    <input type="password" v-model="creditial.password_confrim" class="form-control" :class="{'is-invalid':errors.password_confrim}">
+                                    <small v-if="errors.password_confrim" class="text text-danger"><strong>{{errors.password_confrim[0]}}</strong></small>
+                               </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-primary mt-3">Register</button>
+                    </form>
+                </div>
+            </div>
+       </div>
+   </div>
+</div>
 </template>
 <script>
+import mixins from '../mixin/mixin'
 export default {
-    name:'Register'
+    mixins:[mixins],
+    name:'Login',
+    data(){
+        return{
+          creditial:{
+              name:'',
+              phone:'',
+              email:'@gmail.com',
+              password:'password',
+              password_confrim:'password'
+          },
+          errors:{}
+        }
+    },
+    methods:{
+        async register(){
+            let url = this.$baseUrl + 'register';
+            let data = await this.sendData(url,this.creditial);
+ 
+            if(data.success){
+                this.$router.push({name:'Login'})
+            }else{
+              this.errors = data.errors
+              console.log(this.errors);
+            }
+        },
+      
+    }
 }
 </script>
